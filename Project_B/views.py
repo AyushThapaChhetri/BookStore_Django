9dc87003d5dab781cb3c02b026e3eec4aab3f26b
AyushThapaChhetri <ayushthapa369@gmail.com>
2025-08-22 23:57:17 +0545

    Before changing the model

--- Project_B/views.py
+++ Project_B/views.py
@@ -6,11 +6,13 @@ from src.cart.models import Cart, CartItem
 def home_view(request):
     cart = None
     items_count = 0
-    
+
     if request.user.is_authenticated:
-        cart = Cart.objects.get(user=request.user)
-        items_count = CartItem.objects.filter(cart=cart).count()
-    return render(request, 'base/base.html', {'cart': cart, 'items_count': items_count})
+        cart = Cart.objects.filter(user=request.user).first()
+        if cart:
+            items_count = CartItem.objects.filter(cart=cart).count()
+
+        return render(request, 'base/base.html', {'cart': cart, 'items_count': items_count})
 
 
 def about_view(request):
--- src/core/models.py
+++ src/core/models.py
@@ -5,36 +5,6 @@ from django.db import models
 from src.core.soft_delete import SafeDeleteModel
 
 
-# Custom manager to exclude soft-deleted items by default.
-# class BaseManger(models.Manager):
-#     # overrides Django's built-in method called get_queryset()
-#     def get_queryset(self):
-#         # only return rows where deleted_at is NULL(i.e., not deleted).
-#         return super().get_queryset().filter(deleted_at__isnull=True)
-
-
-# class AbstractBaseModel(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     deleted_at = models.DateTimeField(blank=True, null=True)
-#
-#     objects = BaseManger() # Default manager excludes soft-deleted items
-#     all_objects = models.Manager()  # Access all items, including deleted ones
-#
-#     def delete(self, *args, **kwargs):
-#         """Soft delete by setting deleted_at"""
-#         self.deleted_at = timezone.now()
-#         self.save()
-#
-#     def restore(self):
-#         """Restore by clearing deleted_at"""
-#         self.deleted_at = None
-#         self.save()
-#     class Meta:
-#         abstract = True #This model won't create a database table
-
-
 class AbstractBaseModel(SafeDeleteModel):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
